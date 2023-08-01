8b89fd0143e5fa3700ae9510ca6611d29f72943a
[Misaka] <[liumengyuan648@gmail.com]>
2023-08-01 15:08:01 +0100

    heatmap added, corr_Matrix updated

---
+++ dataset/correlationMatrix/PSF.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+from enum import IntEnum
+from operator import length_hint
+import numpy as np
+from PIL import Image
+from typing import Optional
+from scipy.signal import correlate
+
+
+
+num_points = 1
+W, H = 20, 20 # x, y axis
+Length = 150 # z axis
+radius = 2.0 # radius on focal plane
+# Vz = 15 # velocity max caculated with V(y, z), flows through x-axis
+Zr = 80 # Rayleigh length
+
+intensity = 125 # The max intensity, 'a' in Gaussian blob
+
+win_size = 20
+img1 = np.zeros([W, H])
+
+# Generate two arrays the same shape as the image.
+# In pixX, every array element has a value equal to the x coordinate of that pixel
+# In pixY, every array element has a value equal to the y coordinate of that pixel
+pixX, pixY = np.meshgrid(np.arange(W), np.arange(H))
+
+class Point:
+    def __init__(self, size: float, dz: Optional[int] = None):
+        self.x = 10
+        self.y = 10
+        self.z = 0
+        self.dz = dz if dz is not None else 0
+        self.size = size * np.sqrt(1 + ((self.z - self.dz) / Zr) ** 2)  # + 0.02 * self.z
+        # self.size = size
+        self.intensity = intensity 
+
+
+def drawPoint(img, point: Point):
+    half_size = point.size
+    w, h = img.shape
+    x = point.x 
+    y = point.y 
+
+
+    #draw particles in blob
+    img += point.intensity * ((np.sqrt(2 * np.pi) / half_size) ** 2) * np.exp(-(((x - pixX.T) / half_size)**2 + ((y - pixY.T) / half_size)**2))
+    img = np.clip(img, 0, 255)
+
+
+import os
+
+# Check if the directories exist, and if not, create them
+os.makedirs('./PSF', exist_ok=True)
+
+correlation_stack = []
+for dz in range(-Length*2, Length*2 + 1, 10):  # Iterate over possible dz values
+    point_groups = []
+    points = [Point(radius, dz=dz) for _ in range(num_points)]  # Initialize points with the current dz
+    point_groups.append(points)
+
+    # Initialize the images
+    img1 = np.zeros([W, H])
+
+    # Update and draw points
+    for points in point_groups:
+        for p in points:
+            drawPoint(img1, p)
+
+    a_win = img1 [:win_size, :win_size].copy()
+    b_win = img1 [:win_size, :win_size].copy()
+    cross_corr = correlate(b_win - b_win.mean(), a_win - a_win.mean(), method="fft")
+
+    correlation_stack.append(cross_corr)
+
+    # Save the images
+    # Normalize to [0, 1]
+    normalized_array = (cross_corr - cross_corr.min()) / (cross_corr.max() - cross_corr.min())
+
+    # Save as a 32-bit floating point TIFF image
+    Image.fromarray(normalized_array.T.astype(np.float32)).save(f'./PSF/dz_{dz + 300}.tif')
+   
+
+# Convert the list of 2D arrays into a 3D numpy array
+correlation_stack = np.stack(correlation_stack)
+
+np.save('PSF.npy', correlation_stack)
+# data = np.load('filename.npy')
+
+
---
+++ dataset/correlationMatrix/correlationMatrix.py
@@ -0,0 +1,161 @@
+import matplotlib.pyplot as plt
+from enum import IntEnum
+from operator import length_hint
+from matplotlib import image
+from PIL import Image
+from typing import Optional
+import numpy as np
+
+from scipy.signal import correlate
+
+# load images
+objectA = np.load('stkA.npy')
+objectB = np.load('stkB.npy')
+
+win_size = 369
+
+import os
+# Check if the directories exist, and if not, create them
+os.makedirs('./CorrelationMatrix', exist_ok=True)
+
+# correlation_stack = []
+
+# for i in range(objectA.shape[0]):  # Iterate over possible dz values
+    
+#     img1 = objectA[i]
+#     img2 = objectB[i]
+
+#     a_win = img1 [:win_size, :win_size].copy()
+#     b_win = img2 [:win_size, :win_size].copy()
+
+#     cross_corr = correlate(b_win - b_win.mean(), a_win - a_win.mean(), method="fft")
+
+#     correlation_stack.append(cross_corr)
+
+#     # Save the images
+#     Image.fromarray(cross_corr.T.astype(np.uint8)).save(f'./CorrelationMatrix/dz_{i * 10}.bmp')
+
+
+
+# # load images
+# objectA = np.load('stkA.npy')
+# objectB = np.load('stkB.npy')
+
+# int_win_size = 32
+# search_win_size = 36 
+
+# img1 = objectA[0]
+# img2 = objectB[0]
+
+# a_win = img1[:int_win_size, :int_win_size].copy()
+# b_win = img2[:search_win_size, :search_win_size].copy()
+
+# cross_corr = correlate(b_win - b_win.mean(), a_win - a_win.mean(), mode="same", method="fft")
+# print(cross_corr.shape)
+# # cross_corr shape in (36, 36)
+
+
+# np.save('corrMatrix.npy', correlation_stack)
+# data = np.load('filename.npy')
+
+# from mpl_toolkits.mplot3d import Axes3D
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection="3d")
+# Y, X = np.meshgrid(np.arange(cross_corr.shape[0]), np.arange(cross_corr.shape[1]))
+
+# ax.plot_surface(Y, X, correlation_stack[0], cmap='jet', linewidth=0.2)  # type: ignore
+# plt.title("Correlation map — peak is the most probable shift")
+# plt.show()
+
+
+
+int_win_size = np.array([32, 32])
+search_win_size = np.array([36, 36])
+half_int_win_size = int_win_size // 2
+half_search_win_size = search_win_size // 2
+corr_win_size = search_win_size - int_win_size + 1
+
+def correlate_and_combine(
+    img1, img2, half_int_win_size, half_search_win_size
+):
+
+    total_size_y = img1.shape[0] // int_win_size[0] * corr_win_size[0]
+    total_size_x = img1.shape[1] // int_win_size[1] * corr_win_size[1]
+    # initialize 
+    correlation_array = np.zeros((total_size_y, total_size_x))
+    
+    ys = np.arange(half_int_win_size[0], img1.shape[0], 2 * half_int_win_size[0])
+    xs = np.arange(half_int_win_size[1], img1.shape[1], 2 * half_int_win_size[1])
+    dys = np.zeros((len(ys), len(xs)))
+    dxs = np.zeros((len(ys), len(xs)))
+    for iy, y in enumerate(ys):
+        for ix, x in enumerate(xs):
+            int_win = img1[
+                y - half_int_win_size[0] : y + half_int_win_size[0],
+                x - half_int_win_size[1] : x + half_int_win_size[1],
+            ]
+            search_win_y_min = y - half_search_win_size[0]
+            search_win_y_max = y + half_search_win_size[0]
+            search_win_x_min = x - half_search_win_size[1]
+            search_win_x_max = x + half_search_win_size[1]
+            truncated_search_win = img2[
+                max(0, search_win_y_min) : min(img2.shape[0], search_win_y_max),
+                max(0, search_win_x_min) : min(img2.shape[1], search_win_x_max),
+            ]
+            cross_corr = correlate(
+                truncated_search_win - np.mean(truncated_search_win),
+                int_win - np.mean(int_win),
+                mode="valid",
+                method="fft",
+            )
+            
+            start_y = iy * cross_corr.shape[0]
+            end_y = start_y + cross_corr.shape[0]
+            start_x = ix * cross_corr.shape[1]
+            end_x = start_x + cross_corr.shape[1]
+    
+            # place the result in correct location
+            correlation_array[start_y: end_y, start_x: end_x] = cross_corr
+    return correlation_array
+
+
+correlation_stack = []
+
+for i in range(objectA.shape[0]):  # Iterate over possible dz values
+    
+    img1 = objectA[i]
+    img2 = objectB[i]
+
+    correlation_array = correlate_and_combine(
+        img1=img1, 
+        img2=img2, 
+        half_int_win_size=half_int_win_size, 
+        half_search_win_size=half_search_win_size
+    )
+
+    correlation_stack.append(correlation_array)
+
+    # Save the images
+    # Normalize to [0, 1]
+    normalized_array = (correlation_array - correlation_array.min()) / (correlation_array.max() - correlation_array.min())
+
+    # Save as a 32-bit floating point TIFF image
+    Image.fromarray(normalized_array.T.astype(np.float32)).save(f'./CorrelationMatrix/dz_{i * 10}.tif')
+
+print(correlation_array.shape)
+
+
+correlation_stack = np.stack(correlation_stack)
+np.save('corrMatrix.npy', correlation_stack)
+# data = np.load('filename.npy')
+
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+Y, X = np.meshgrid(np.arange(correlation_array.shape[0]), np.arange(correlation_array.shape[1]))
+
+ax.plot_surface(Y, X, correlation_stack[0].T, cmap='jet', linewidth=0.2)  # type: ignore
+plt.title("Correlation map — peak is the most probable shift")
+plt.show()
